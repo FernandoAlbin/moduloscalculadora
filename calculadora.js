4e42cea14a5ac2978ae774b5526f48697e93f578
Fernando Albino <fernando.albinoo@hotmail.com>
2023-08-31 09:13:48 -0300

    Fernandoo

--- calculadora.js
+++ calculadora.js
@@ -3,9 +3,10 @@ var media = require("./modulos/media");
 var multiplica = require ("./modulos/multiplica");
 var divisao = require ("./modulos/divisao");
 var mensagem = require ("./modulos/mensagem");
+var nome = "Fernando";
 
 console.log("A soma de X e Y é: " + somar(20,90));
 console.log("A média de X e Y é: " + media(20,90));
 console.log("O produto entre X e Y é: " + multiplica(20,90));
 console.log("A divisão entre X e Y é: " + divisao(20,90));
-console.log(mensagem(" Fernando"));
+console.log(mensagem(nome));
